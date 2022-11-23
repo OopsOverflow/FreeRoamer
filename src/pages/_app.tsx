be91043d51f694e29946e8ab3d1533668e0b568b
OopsOverflow <houssemmahmoud18@gmail.com>
2022-11-23 12:26:12 +0100

    Add prisma and NextAuth

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/globals.css';
+import { SessionProvider } from "next-auth/react"
+import type { AppProps } from 'next/app';
+
+export default function App({  Component,  pageProps: { session, ...pageProps }} 
+  : {
+    Component: React.FC,
+    pageProps: any,
+  }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
