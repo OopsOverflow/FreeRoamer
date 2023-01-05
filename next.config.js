7dcc4c3e99ebdda4eed4af28cc1f9c03dd107801
OopsOverflow <houssemmahmoud18@gmail.com>
2023-01-05 20:21:18 +0100

    Project Config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+    AWS_BUCKET_REGION: process.env.AWS_BUCKET_REGION,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+  },
 };
 
 module.exports = nextConfig;
